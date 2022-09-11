48ec36e732684d21adcfcb1de5c40a6c48497ef1
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-09-11 19:56:11 +0100

    Added Roll to generated random numbers

--- src/App.js
+++ src/App.js
@@ -10,19 +10,26 @@ function App() {
   function allNewDice() {
     let newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6));
+      newDice.push({ value: Math.ceil(Math.random() * 6), isHeld: false });
     }
     return newDice;
   }
   console.log(allNewDice());
 
   const diceElements = dice.map((die) => (
-    <Dice key={Math.random()} value={die} />
+    <Dice key={Math.random()} value={die.value} />
   ));
+
+  const rollHandler = () => {
+    setDice(allNewDice());
+  };
 
   return (
     <main className='container'>
       <div className='dice_container'>{diceElements}</div>
+      <button className='btn' onClick={rollHandler}>
+        Roll
+      </button>
     </main>
   );
 }
