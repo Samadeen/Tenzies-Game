43824ee78812f923ed530af39c01fe2b7041f1bc
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-09-11 21:14:02 +0100

    Conditional Styling

--- src/Dice.js
+++ src/Dice.js
@@ -1,8 +1,11 @@
 import './App.css';
 
 const Dice = (props) => {
+  const styles = {
+    backgroundColor: props.isHeld ? '#59e391' : '#fff',
+  };
   return (
-    <div className='die_face'>
+    <div className='die_face' style={styles} onClick={props.holdDice}>
       <h2 className='die_num'>{props.value}</h2>
     </div>
   );
